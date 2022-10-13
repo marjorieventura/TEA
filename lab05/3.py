2e7765df46aaf1f607b2305b749fb3d1da583362
marjorieventura <112130511+marjorieventura@users.noreply.github.com>
2022-10-12 22:48:18 -0600

    Delete lab05 directory

--- lab05/3.py
+++
@@ -1,8 +0,0 @@
-try:
-  entrada = input("Ingrese el nombre del archivo: ")
-  archivo = open(entrada, "r", encoding="UTF-8")
-  for linea in archivo:
-    print(linea.upper())
-except:
-  print("Error, archivo no existe")
-  
